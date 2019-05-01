2ba9ec4a995a12aa3491504e41bd639b5cf7e3ec
sergi <sergi@sergi-VirtualBox>
2019-05-01 12:50:56 +0200

    Versió 0.2 del projecte prjava01”.

--- src/prjava01/prjava01.java
+++ src/prjava01/prjava01.java
@@ -14,6 +14,8 @@ public class prjava01 {
     public static void main(String[] args) throws IOException {
         File f = new File("fitxer.html");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write(" <h1>DAW2 m08uf4eac1</h1>");
+            bw.newLine(); 
             bw.write("<html>");
             bw.newLine();
             bw.write("  <head>");
